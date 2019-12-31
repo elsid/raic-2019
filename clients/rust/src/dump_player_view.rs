9625424cc7e6bd3b286d5e4ed4fec0ad11921f16
elsid <elsid.mail@gmail.com>
2019-12-31 15:55:54 +0100

    Add rust binary to dump player view as JSON

---
+++ clients/rust/src/dump_player_view.rs
@@ -0,0 +1,54 @@
+extern crate rustc_serialize;
+
+struct Args {
+    host: String,
+    port: u16,
+    token: String,
+}
+
+impl Args {
+    fn parse() -> Self {
+        let mut args = std::env::args();
+        args.next().unwrap();
+        let host = args.next().unwrap_or("127.0.0.1".to_owned());
+        let port = args
+            .next()
+            .map_or(31001, |s| s.parse().expect("Can't parse port"));
+        let token = args.next().unwrap_or("0000000000000000".to_string());
+        Self { host, port, token }
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    use trans::Trans;
+    use std::io::Write;
+
+    let args = Args::parse();
+    let stream = std::net::TcpStream::connect((args.host.as_str(), args.port))?;
+
+    stream.set_nodelay(true)?;
+
+    let stream_clone = stream.try_clone()?;
+    let mut reader = std::io::BufReader::new(stream);
+    let mut writer = std::io::BufWriter::new(stream_clone);
+
+    args.token.write_to(&mut writer)?;
+    writer.flush()?;
+
+    loop {
+        let message = model::ServerMessageGame::read_from(&mut reader)?;
+        let player_view = match message.player_view {
+            Some(view) => view,
+            None => break,
+        };
+        let json = rustc_serialize::json::encode(&player_view).expect("Can't serialize player_view");
+        write!(&mut std::io::stdout(), "{}\n", json)?;
+        let message = model::PlayerMessageGame::ActionMessage {
+            action: model::Versioned { inner: std::collections::HashMap::new() },
+        };
+        message.write_to(&mut writer)?;
+        writer.flush()?;
+    }
+
+    Ok(())
+}
